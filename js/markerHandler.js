83c2463296221ddb1e2efdc2ad436bf91e85c828
tabithavimal21 <95914849+tabithavimal21@users.noreply.github.com>
2022-06-26 14:04:47 +0530

    Add files via upload

---
+++ js/markerHandler.js
@@ -0,0 +1,35 @@
+AFRAME.registerComponent("marker-handler",{
+    init:async function(){
+        this.el.addEventListener("markerFound",()=>{
+            console.log("Marker found")
+            var buttonDiv=document.getElementById("button-div")
+            buttonDiv.style.display="flex"
+            var ratingButton=document.getElementById("rating-button")
+            var orderButton=document.getElementById("order-button")
+            ratingButton.addEventListener("click",()=>{
+                swal({
+                    icon:"warning",
+                    title:"Rate The Dish",
+                    text:"We would love to hear you rate this dish",
+                })
+
+            })
+            orderButton.addEventListener("click",()=>{
+                swal({
+                    icon:"https://i.imgur.com/4NZ6uLY.jpg",
+                    title:"Order Summary",
+                    text:"Work In Progress",
+
+                })
+            })
+
+        })
+
+        this.el.addEventListener("markerLost",()=>{
+            console.log("marker lost")
+            var buttonDiv=document.getElementById("button-div")
+            buttonDiv.style.display="none"
+            
+        })
+    }
+})
